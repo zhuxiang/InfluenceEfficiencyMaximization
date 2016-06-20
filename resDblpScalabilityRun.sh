1a87f4310e270b9f3112b84d685999f82e436a54
Xiang Zhu <zhuxiang19881117@gmail.com>
2016-06-21 00:19:14 +0800

    fix the .sh for scalability test.

--- resDblpScalabilityRun.sh
+++ resDblpScalabilityRun.sh
@@ -1,8 +1,8 @@
-java -jar scalability-p0.01.jar data/dblp.txt 1000 wic > result/res-dblp-wic-1000.out
-java -jar scalability-p0.01.jar data/dblp.txt 10000 wic > result/res-dblp-wic-10000.out
-java -jar scalability-p0.01.jar data/dblp.txt 100000 wic > result/res-dblp-wic-100000.out
-java -jar scalability-p0.01.jar data/dblp.txt 1000000 wic > result/res-dblp-wic-1000000.out
-java -jar scalability-p0.01.jar data/dblp.txt 1000 uic > result/res-dblp-uic-1000.out
-java -jar scalability-p0.01.jar data/dblp.txt 10000 uic > result/res-dblp-uic-10000.out
-java -jar scalability-p0.01.jar data/dblp.txt 100000 uic > result/res-dblp-uic-100000.out
-java -jar scalability-p0.01.jar data/dblp.txt 1000000 uic > result/res-dblp-uic-1000000.out
+java -jar scalability-p0.01.jar data/dblp.txt 1000 wic 50 > result/res-dblp-wic-1000.out
+java -jar scalability-p0.01.jar data/dblp.txt 10000 wic 50 > result/res-dblp-wic-10000.out
+java -jar scalability-p0.01.jar data/dblp.txt 100000 wic 50 > result/res-dblp-wic-100000.out
+java -jar scalability-p0.01.jar data/dblp.txt 1000000 wic 50 > result/res-dblp-wic-1000000.out
+java -jar scalability-p0.01.jar data/dblp.txt 1000 uic 50 > result/res-dblp-uic-1000.out
+java -jar scalability-p0.01.jar data/dblp.txt 10000 uic 50 > result/res-dblp-uic-10000.out
+java -jar scalability-p0.01.jar data/dblp.txt 100000 uic 50 > result/res-dblp-uic-100000.out
+java -jar scalability-p0.01.jar data/dblp.txt 1000000 uic 50 > result/res-dblp-uic-1000000.out
